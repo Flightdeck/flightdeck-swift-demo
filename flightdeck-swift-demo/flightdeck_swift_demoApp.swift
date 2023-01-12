519620243107beb02716483e0df4593b0dc0a691
jvannistelrooy <jvannistelrooy@gmail.com>
2023-01-12 20:25:04 +0100

    Initial Commit

---
+++ flightdeck-swift-demo/flightdeck_swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  flightdeck_swift_demoApp.swift
+//  flightdeck-swift-demo
+//
+//  Created by Jasper van Nistelrooy on 12/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct flightdeck_swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
